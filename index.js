cfde90aa900c15128e0418deaec3cfe3dcbb9736
nemostatus <mattcorralejo1@gmail.com>
2021-02-09 09:03:14 -0800

    created an onchange handler and listener for the dropdown menu. need to adown. text to speech needed.

--- index.js
+++ index.js
@@ -4,3 +4,20 @@ let voices = synth.getVoices()
  let utterThis = new SpeechSynthesisUtterance(message)
     utterThis.voice = voices[1]
 synth.speak(utterThis)}
+
+function changeFunc() {
+    var selectBox = document.getElementById("questions");
+    var selectedValue = selectBox.options[selectBox.selectedIndex].value;
+   let box = document.getElementById("message")
+  switch(selectedValue){
+      case "about":
+      box.innerHTML = "Matthew is a full stack developer who created me and the other projects below.";
+      break;
+      case "maxine":
+        box.innerHTML = "I'm maxine. I'm here to help you navigate matthew's portfolio. ";
+        break;
+        case "nav":
+            box.innerHTML = "Choose any option to get info on Matt's portfolio ";
+            break;
+  }
+   }
